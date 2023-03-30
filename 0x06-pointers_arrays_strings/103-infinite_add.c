0b6dcf76706bfa542aaf7d0382e8dafe50bbb8fe
Shaylee21 <shayleepillay021@gmail.com>
2023-03-30 13:27:37 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * infinite_add - Adds two positive numbers.
+ * @n1: A pointer to the first number as a string.
+ * @n2: A pointer to the second number as a string.
+ * @r: A pointer to the buffer where the results will be stored.
+ * @size_r: The size of the buffer @r.
+ *
+ * Return: If the sum can be stored in @r, a pointer to the buffer @r.
+ * otherwise, return 0.
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
