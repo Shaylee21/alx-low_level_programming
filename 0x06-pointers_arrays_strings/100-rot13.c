943b4e483fc3830c8b23f9f8ea396f9115fdbaeb
Shaylee21 <shayleepillay021@gmail.com>
2023-03-30 15:28:32 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+/**
+ * rot13 - encodes  string in rot13.
+ * @s: string to be encoded
+ *
+ * Return: The end result string.
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int k;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (k = 0; a[k] != '\0'; k++)
+	{
+	if (s[i] == a[k])
+	{
+	s[i] = b[k];
+	break;
+	}
+	}
+	}
+
+	return (s);
+}
