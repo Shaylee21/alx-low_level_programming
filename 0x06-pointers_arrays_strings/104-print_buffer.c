584e0d689d452b20c2afbf3928656f5f92521241
Shaylee21 <shayleepillay021@gmail.com>
2023-03-30 14:41:26 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,33 +1,58 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_buffer - Prints the content of a bbuffer
- * @b: pointer to buffer
- * @size: Soze of the buffer
+ * print_line - prints a s bytes of a buffer
+ * @a: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
  *
- * Return; Void
+ * Return: void
  */
 
+void print_line(char *a, int s, int l)
+{
+int j, n;
+for (j = 0; j <= 9; j++)
+{
+if (j <= s)
+printf("%02x", a[l * 10 + j]);
+else
+printf("  ");
+if (j % 2)
+putchar(' ');
+}
+for (n = 0; n <= s; n++)
+{
+if (a[l * 10 + n] > 31 && a[l * 10 + n] < 127)
+putchar(a[l * 10 + n]);
+else
+putchar('.');
+}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
 void print_buffer(char *b, int size)
 {
-	int i, j;
-
-	if (size <= 0)
-	{
-	_putchar('\n');
-	return;
-	}
-
-	for (i = 0; i < size; i += 10)
-	{
-	print_hex(i);
-	_putchar(';');
-	_putchar(' ');
-	for (j = i; j < i + 10; j++)
-	print_hexhebyte(j < size ? b[j] : 0);
-	_putchar(' ');
-	for (j = i; j < i + 10 && j < size; j++)
-	_putchar(isprint(b[j]) : '.');
-	_putchar('\n');
-	}
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 10 - 1, i);
+}
+putchar('\n');
+}
+if (size == 0)
+putchar('\n');
 }
