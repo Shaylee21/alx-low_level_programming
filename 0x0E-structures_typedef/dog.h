97234d8f856be7ba7026e0f2b293671e34c031ad
Shaylee21 <shayleepillay021@gmail.com>
2023-04-17 12:56:43 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of dogs owner
+ *
+ * Description: structure representing a dog etc
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
