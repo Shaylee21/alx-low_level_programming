73862bec0b132f88c26163ad440c8cb90c7a0e8b
Shaylee21 <shayleepillay021@gmail.com>
2023-04-28 07:34:46 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * print_m - Prints a message
+ */
+
+void print_m(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
