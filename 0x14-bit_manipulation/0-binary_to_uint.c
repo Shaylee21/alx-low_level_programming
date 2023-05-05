713f7090c949ae0dd58b087613e14e5cf5dedabc
Shaylee21 <shayleepillay021@gmail.com>
2023-05-05 04:25:36 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x = 0;
-	int y;
+	int i;
+	unsigned int dec_val = 0;
 
 	if (!b)
-	return (0);
-
-	for (y = 0; b[y]; y++)
-	{
-		if (b[y] < '0' || b[y] > '1')
 		return (0);
 
-		x = 2 * x + (b[y] - '0');
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (x);
+	return (dec_val);
 }
